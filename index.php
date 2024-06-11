80df3cc41038c47c4bdd235f8f29d1f3a527a598
m-mesbah <m.mesbah@4tel.sa>
2024-06-11 19:39:56 +0300

    edit in index m-md-3

--- index.php
+++ index.php
@@ -25,19 +25,19 @@ require_once("./includes/nav.php");
             <div class="row  justify-content-between align-items-around my-5" dir='rtl'>
                 
             
-                <a href="<?php echo  $_SESSION['domain'] ?>/views/quraan/read.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none m-md-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
+                <a href="<?php echo  $_SESSION['domain'] ?>/views/quraan/read.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none mx-md-3 my-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
                     <h2 class="text-white">القرآن الكريم</h2>
                 </a>
-                <a href="<?php echo  $_SESSION['domain'] ?>/views/quraan/listen.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none m-md-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
+                <a href="<?php echo  $_SESSION['domain'] ?>/views/quraan/listen.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none mx-md-3 my-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
                     <h2 class="text-white">القرآن الكريم صوت</h2>
                 </a>
-                <a  href="<?php echo  $_SESSION['domain'] ?>/views/quraan/azkaar.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none m-md-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
+                <a  href="<?php echo  $_SESSION['domain'] ?>/views/quraan/azkaar.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none mx-md-3 my-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
                     <h2 class="text-white">اذكار</h2>
                 </a>
-                <a  href="<?php echo  $_SESSION['domain'] ?>/views/quraan/ahadeeth.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none m-md-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
+                <a  href="<?php echo  $_SESSION['domain'] ?>/views/quraan/ahadeeth.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none mx-md-3 my-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
                     <h2 class="text-white">احاديث</h2>
                 </a>
-                <a href="<?php echo  $_SESSION['domain'] ?>/views/quraan/eza3a.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none m-md-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
+                <a href="<?php echo  $_SESSION['domain'] ?>/views/quraan/eza3a.php" class="col-md-5  bg-success  text-center  align-content-center text-decoration-none mx-md-3 my-3" style='height: 200px; border-color: transparent; border-width: 10px; ' >
                     <h2 class="text-white">الاذاعات</h2>
                 </a>
                 
